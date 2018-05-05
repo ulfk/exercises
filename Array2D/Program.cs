a194785d0c1feee3cf231fa27330576dc18019da
Ulf Kühnle <ulf.kuehnle@gmx.de>
2018-05-05 14:46:28 +0200

    Added first two exercises

---
+++ Array2D/Program.cs
@@ -0,0 +1,77 @@
+﻿/*
+ * Lösung zu dieser Aufgabenstellung:
+ * https://trainyourprogrammer.de/csharp-199-ermittlung-von-zeilen--und-spaltennummer-eines-2d-array-feldes.html
+ * 
+ * Die Felder eines "unendlich" großen 2D-Arrays seien nach folgendem "Diagonal-Schema" nummeriert:
+ *
+ * 1 2 4 7 . . .
+ * 3 5 8 . . . .
+ * 6 9 . . . . .
+ * 10 . . . . . .
+ * . . . . . . usw.
+ * 
+ * Man schreibe ein Programm, das für eine gegebene Feldnummer N die Zeilen- (ZN) und die Spaltennummer (SN) zurückgibt.
+ * Zeilen- und Spaltennummerierungen beginnen wie üblich mit 0.
+ * 
+ * Beispiele:
+ * N = 8 -> ZN = 1, SN = 2
+ * N = 31 -> ZN = 2, SN = 5 
+ * 
+ */
+
+namespace Csharp_199_2DArray
+{
+    using System;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputString;
+            // loop to enter and check several numbers in a row
+            do
+            {
+                Console.Write("Enter integer number or 'Q' to quit: ");
+                inputString = Console.ReadLine();
+                if(int.TryParse(inputString,out int inputNumber))
+                {
+                    CalculateArrayIndexes(inputNumber, out int resultColumn, out int resultRow);
+                    Console.WriteLine($"Number={inputNumber}  -> Result: Column={resultColumn} Row={resultRow}");
+                }
+            } while (inputString.ToLower() != "q");
+        }
+
+        static void CalculateArrayIndexes(int inputNumber, out int resultColumn, out int resultRow)
+        {
+            resultColumn = 0;
+            resultRow = 0;
+
+            // value to be added when stepping from one diagonal to the next one
+            var addValue = 1;
+            // first and last value in a diagonal
+            var firstValueOfDiagonal = 1;
+            var lastValueOfDiagonal = 1;
+
+            var continueSearch = true;
+            while (continueSearch)
+            {
+                // check if given number is on this diagonal
+                if (  inputNumber >= firstValueOfDiagonal 
+                    && inputNumber <= lastValueOfDiagonal)
+                {
+                    // calculate the index values and terminate loop
+                    resultColumn = lastValueOfDiagonal - inputNumber;
+                    resultRow = inputNumber - firstValueOfDiagonal;
+                    continueSearch = false;
+                }
+                else
+                {
+                    // step to next diagonal
+                    firstValueOfDiagonal += addValue;
+                    lastValueOfDiagonal += addValue + 1;
+                    addValue++;
+                }
+            }
+        }
+    }
+}
---
+++ BinaryDecimalConverter/Program.cs
@@ -0,0 +1,63 @@
+﻿/*
+ * Lösung zu dieser Aufgabenstellung:
+ * https://trainyourprogrammer.de/csharp-189-zahlen-umwandeln-binaer-zu-dezimal.html
+ * 
+ * Schreibe eine Funktion, die Zahlen aus dem Dualsystem in Zahlen des Dezimalsystems umwandelt.
+ *
+ * Beispiel:
+ * Binär: 11010
+ * Dezimal: 26 
+ * 
+ */
+namespace Csharp_189_BinaryDecimalConverter
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputString;
+            // loop to enter and check several numbers in a row
+            do
+            {
+                Console.Write("Enter binary number or 'Q' to quit: ");
+                inputString = Console.ReadLine();
+                if (IsValidBinaryNumber(inputString))
+                {
+                    var resultInteger = ConvertBinaryToDecimal(inputString);
+                    Console.WriteLine($"Binary={inputString}  -->  Decimal={resultInteger}\n");
+                }
+                else
+                {
+                    Console.WriteLine("ERROR: Invalid input! \n" +
+                                      "   Allowed characters: '0' and '1'\n" +
+                                      "   Minimum length: 1 character\n" +  
+                                      "   Maximum length: 31 characters\n" + 
+                                      "   Example input:  100110\n");
+                }
+            } while (inputString.ToLower() != "q");
+        }
+
+        static int ConvertBinaryToDecimal(string binaryString)
+        {
+            var result = 0;
+            var length = binaryString.Length;
+            for(var idx = 0; idx < length; idx++)
+            {
+                // get digits from right to left and convert them to decimal
+                var value = binaryString[length - (idx + 1)] == '1' ? 1 : 0;
+                result += value * (1 << idx);
+            }
+
+            return result;
+        }
+
+        static bool IsValidBinaryNumber(string binaryString)
+        {
+            return binaryString.Length < 32
+                && Regex.IsMatch(binaryString, @"^([01]+)$");
+        }
+    }
+}
